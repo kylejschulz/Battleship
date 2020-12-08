7faa61a3cd0c1a0a673377456f9ee591bf0916da
kylejschulz <kylschulz@gmail.com>
2020-12-08 11:57:29 -0600

    dd cell class

---
+++ lib/cell.rb
@@ -0,0 +1,26 @@
+require './lib/ship'
+class Cell
+  attr_reader :coordinate, :ship
+  def initialize(coordinate, ship = nil)
+    @coordinate = coordinate
+    @ship = ship
+  end
+
+  def empty?
+    @ship == nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @health != @length
+  end
+
+  def fire_upon
+  #  if fired_upon? #&& (@coordinate.empty? == false)
+      ship.health -= 1
+
+  end
+end
