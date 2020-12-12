6e8772fc974337d6e9a9de3598c4b36e4df722ed
Jacob Arellano <jmm.arellano@gmail.com>
2020-12-12 09:42:46 -0700

    add loop to start game

---
+++ runner.rb
@@ -0,0 +1,18 @@
+require './lib/game'
+
+
+  puts "Welcome to BATTLESHIP
+  Enter p to play. Enter q to quit."
+
+loop do
+  player_choice = gets.chomp
+
+  if player_choice == "q"
+  "See you next time"
+    break
+  elsif player_choice == "p"
+    game.start
+  else player_choice != "q" || "p"
+    puts "select again"
+  end
+end
