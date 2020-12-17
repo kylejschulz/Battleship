af09478f4b3911a9c5392c64f62a68f1387b8512
Jordan Beck <jordanfbeck@gmail.com>
2020-12-16 17:30:16 -0700

    Add changes to game text

--- lib/player_messages.rb
+++ lib/player_messages.rb
@@ -10,15 +10,15 @@ class PlayerMessages
 
   def human_cruiser_text
     puts "Computer player has placed their ships."
-    puts "Enter the location for your Cruiser (3 consecutive spaces): like this A1 B1 C1 "
+    puts "Enter the location for your Cruiser (3 consecutive spaces): Like this A1 B1 C1 "
   end
 
   def wrong_coordinate
-    puts "That's not going to work, please try again"
+    puts "That's not going to work, please try again..."
   end
 
   def human_submarine_text
-    puts "Please enter your locations for the Submarine (2 consecutive spaces): like this D1 D2 "
+    puts "Please enter your locations for the Submarine (2 consecutive spaces): Like this D1 D2 "
   end
 
   def computer_header
@@ -30,19 +30,19 @@ class PlayerMessages
   end
 
   def enter_coordinates
-    puts "enter your coordinate: "
+    puts "Enter your coordinate: "
   end
 
   def player_loss
-    puts "way to lose to a computer"
+    puts "Way to lose to a computer, on easy mode!"
   end
 
   def player_win
-    puts "congrats on beating a computer"
+    puts "Congrats on beating a computer!"
   end
 
   def play_again
-    puts "play again? (y/n)? "
+    puts "Play again? (y/n)? "
   end
 
   def thanks
